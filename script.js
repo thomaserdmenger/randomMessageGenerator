e70ab7cc18ccecef3f34c630bb0b4a7549211111
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-04-23 16:37:51 +0200

    build jukebox and functionality

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
 // Random Message Generator
 
 const message = {
-    zodiac: ['ram', 'bull', 'twins', 'crab', 'lion', 'virgin', 'balance', 'scorpion', 'archer', 'goat', 'water bearer', 'fish'],
-    words: ['agile', 'energetic', 'cheeky', 'smart', 'foolish', 'quiet', 'lazy', 'tired', 'hungry'],
-    friends: ['loved', 'hated', 'missed', 'feeded', 'kicked', 'punshed', 'instructed'],
-    randNum(arr) {
-        return Math.floor(Math.random() * arr.length);
-    },
-    ranZodiac() {
-        const randZodiac = this.zodiac[this.randNum(this.zodiac)];
-        return randZodiac;
-    },
-    randWords() {
-        const randWords = this.words[this.randNum(this.words)];
-        return randWords;
-    },
-    randFriends() {
-        const randFriends = this.friends[this.randNum(this.friends)];
-        return randFriends;
-    },
-    randMessage() {
-        return `The ${this.ranZodiac()} is ${this.randWords()} and ${this.randWords()}. That is why he is ${this.randFriends()} by his friends and relatives.`;
-    }
+  zodiac: [
+    'ram',
+    'bull',
+    'twins',
+    'crab',
+    'lion',
+    'virgin',
+    'balance',
+    'scorpion',
+    'archer',
+    'goat',
+    'water bearer',
+    'fish'
+  ],
+  words: ['agile', 'energetic', 'cheeky', 'smart', 'foolish', 'quiet', 'lazy', 'tired', 'hungry'],
+  friends: ['loved', 'hated', 'missed', 'feeded', 'kicked', 'punshed', 'instructed'],
+  randNum(arr) {
+    return Math.floor(Math.random() * arr.length)
+  },
+  ranZodiac() {
+    const randZodiac = this.zodiac[this.randNum(this.zodiac)]
+    return randZodiac
+  },
+  randWords() {
+    const randWords = this.words[this.randNum(this.words)]
+    return randWords
+  },
+  randFriends() {
+    const randFriends = this.friends[this.randNum(this.friends)]
+    return randFriends
+  },
+  randMessage() {
+    return `The ${this.ranZodiac()} is ${this.randWords()} and ${this.randWords()}. That is why he is ${this.randFriends()} by his friends and relatives.`
+  }
 }
 
-console.log(message.randMessage());
+// console.log(message.randMessage())
+
+// Link to HTML
+
+const btn = document.querySelector('.button')
+const text = document.querySelector('span')
+
+const newText = () => {
+  text.innerHTML = message.randMessage()
+}
+
+btn.addEventListener('click', newText)
